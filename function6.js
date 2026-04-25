e997587db47e85457b134cfd896035ba414f6145
leonelaviera-c <leonela.viera@anima.edu.uy>
2026-04-24 21:17:24 -0300

    Function

--- function6.js
+++ function6.js
@@ -1,16 +1,8 @@
-function calcularDoble(n) {
-  return n * 2;
-}
-
 const calcularDoble = (n) => {
   return n * 2;
 };
 
 console.log(calcularDoble(6));
-
-function esMayorDeEdad(edad) {
-  return edad >= 18;
-}
 
 const esMayorDeEdad = (edad) => edad >= 18;
 
